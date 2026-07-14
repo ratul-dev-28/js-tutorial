bbb0b5a0e1afd0b404c8f6b4ad3bc5d355a49b9a
ratul <rat.dev.28@gmail.com>
2026-07-14 10:27:06 +0600

    basic conversions

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,39 @@
+
+// =================== Number Conversion =========================
+console.log();
+
+let score = "32"  // try: "32afa" , null, undefined, true, false
+
+console.log(typeof score)
+
+let scoreValue = Number(score)
+console.log(typeof scoreValue)
+console.log(scoreValue)
+
+
+
+
+
+// =================== Boolean Conversion =========================
+console.log();
+
+let isLoggedIn = true //try: false, 1, 0, "", "1", "0", "1214", "asdf"
+
+console.log(isLoggedIn)
+
+let booleanValue = Boolean(isLoggedIn)
+console.log(typeof booleanValue)
+console.log(booleanValue)
+
+
+
+// ==================== String Conversion ========================
+console.log();
+
+let age = 33; // try other data types
+
+console.log(age)
+
+let ageInString = String(age)
+console.log(typeof ageInString)
+console.log(ageInString)
