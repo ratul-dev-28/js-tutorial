e88f76645b99312cbdb52d6a4ab7bd1af5c2f668
ratul <rat.dev.28@gmail.com>
2026-07-15 12:28:49 +0600

    basic comparisons

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,24 @@
+// ============= compare ssme data type =============
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 2)
+
+// ============= compare different data type =============
+// console.log(null == 0)
+
+// console.log(null > 0)
+// console.log(null >= 0)
+
+// console.log(null < 0)
+// console.log(null <= 0)
+
+
+
+/*
+ comparing with undefined always yeilds 'false'
+*/
+
+// ============= STRICT COMPARISON =============
+// check both value and datatype
+// console.log("3" === 3) 
