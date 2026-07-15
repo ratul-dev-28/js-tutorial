86432764985b9c6b639bc348493b79af3b0b0ac4
ratul <rat.dev.28@gmail.com>
2026-07-15 14:23:05 +0600

    date basics

---
+++ 01_basic/07_date.js
@@ -0,0 +1,33 @@
+// =============== Date ===============
+
+// let today = new Date()
+// console.log(today)
+// console.log(today.toString())
+// console.log(today.toDateString())
+// console.log(today.toLocaleString())
+
+
+// let date = new Date(2023, 0, 23, 5, 3)   
+// console.log(date.toLocaleString())
+
+// let date1 = new Date("2023-1-12")   
+// console.log(date1.toLocaleString())
+
+
+
+// let startTime = new Date(2026, 7, 15, 14, 0)
+// let timeStamp = Math.floor(Date.now()/1000)
+// console.log(timeStamp)
+// console.log(Math.floor(startTime.getTime()/1000))
+
+
+
+// let newDay = new Date(2026, 7, 15, 14, 0)
+// console.log(newDay.getDay())
+// console.log(newDay.getMonth())
+
+// newDay.toLocaleString("default", {
+//     weekday: "long",
+// })
+
+// see more in mdn doc 
